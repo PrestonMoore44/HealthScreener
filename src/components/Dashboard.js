b3a1dec015f91b5b93f152ff352691d3bbb65a6a
Preston Moore <prestonmoore@Prestons-MacBook-Pro.local>
2020-07-03 16:54:55 -0700

    FreedomIsComing

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -3,6 +3,9 @@ import { connect } from 'react-redux'
 import { withRouter } from 'react-router'
 import  Question from './Question'
 import { BsDot } from "react-icons/bs";
+import { GiChessRook, GiBrickWall, GiCubeforce } from "react-icons/gi";
+import { DiCode } from "react-icons/di"
+import { FaCubes } from "react-icons/fa"
 
 class Dashboard extends Component {
   state = {
@@ -41,6 +44,51 @@ class Dashboard extends Component {
             <div className="middleMeThird">
               Partner with Matrix Technology to create frictionless digital customer experiences.
             </div> 
+          </div>
+        </div>
+        <div className="popContainerMain">
+          <div className="popContainerInner">
+            <div className="middleMePopContainer">
+              <div className="middleMePop one">
+                <div className="popTop">
+                  <GiChessRook className="middleIcon"/>
+                </div>
+                <div className="popBottom">
+                  Product Stradgety and Roadmapping
+                </div>
+              </div>
+              <div className="middleMePop two">
+                <div className="popTop">
+                  <GiCubeforce color='white' className="middleIcon"/>
+                </div>
+                <div className="popBottom">
+                  User Design Experience
+                </div>
+              </div>
+              <div className="middleMePop three">
+                <div className="popTop">
+                  <DiCode className="middleIcon"/>
+                </div>
+                <div className="popBottom">
+                   Customer Software Developement
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="secondMid">
+            <div className="secondMidInner">Accelerate your roadmap with a</div>
+            <div className="secondMidInnerTwo">modern skillset and client-centered team</div>
+          </div>
+          <div className="centerMeNarrow">Focus on what matters most by relying on us to staff</div>
+          <div className="centerMeNarrow">the perfect team and manage the details for you.</div>
+          <div className="curved">
+            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320">
+              <path fill="#f3f4f5" fill-opacity="1" d="M0,128L48,133.3C96,139,192,149,288,165.3C384,181,480,203,576,208C672,213,768,203,864,176C960,149,1056,107,1152,106.7C1248,107,1344,149,1392,170.7L1440,192L1440,320L1392,320C1344,320,1248,320,1152,320C1056,320,960,320,864,320C768,320,672,320,576,320C480,320,384,320,288,320C192,320,96,320,48,320L0,320Z"></path>
+            </svg>
+            <div></div>
+            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320">
+              <path fill="#f3f4f5" fill-opacity="1" d="M0,128L48,133.3C96,139,192,149,288,165.3C384,181,480,203,576,208C672,213,768,203,864,176C960,149,1056,107,1152,106.7C1248,107,1344,149,1392,170.7L1440,192L1440,0L1392,0C1344,0,1248,0,1152,0C1056,0,960,0,864,0C768,0,672,0,576,0C480,0,384,0,288,0C192,0,96,0,48,0L0,0Z"></path>
+            </svg>
           </div>
         </div>
 
